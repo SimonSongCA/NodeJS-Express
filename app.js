79f4f6e528c797a932adc2abce847b7fc6f6f622
Simon Song <simonsongca@gmail.com>
2022-01-07 02:35:39 -0800

    新增最后两个middleware的注释

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const createError = require("http-errors");
 
 const app = express();
 
+// this part of middleware should always be the second-to-last one
+// if this part is reached, then it indicates:
+// NO ROUTES WERE MATCHED❌
 app.use((req, res, next) => {
   if (req.method !== "GET") {
     next(createError(405));
@@ -12,6 +15,9 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
+// this part of middleware should always be the last one middleware
+// if this part is reached, then it indicates:
+// middleware is returning an error as part of the result
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.send(err.message);
